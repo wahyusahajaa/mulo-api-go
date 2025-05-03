56f182f74e07cade5354990ae44cc594b347ec0b
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-03 17:37:14 +0700

    change method for verify-email, change endpoint me to profile, and adding new routers for users

--- app/routers/routers.go
+++ app/routers/routers.go
@@ -21,9 +21,14 @@ func ProviderFiberApp(h *handlers.Handlers, fiberLogger fiber.Handler) *fiber.Ap
 	authGroup := v1.Group("auth")
 	authGroup.Post("/login", h.Auth.Login)
 	authGroup.Post("/register", h.Auth.Register)
-	authGroup.Post("/verify-email", h.Middleware.AuthRequired(), h.Auth.VerifyEmail)
+	authGroup.Put("/verify-email", h.Middleware.AuthRequired(), h.Auth.VerifyEmail)
 	authGroup.Post("/resend-code", h.Middleware.AuthRequired(), h.Auth.ResendCodeEmailVerification)
-	authGroup.Get("/me", h.Middleware.AuthRequired(), h.Auth.Me)
+	authGroup.Get("/profile", h.Middleware.AuthRequired(), h.Auth.Profile)
+
+	v1.Get("/users", h.Middleware.AuthRequired(), h.User.GetUsers)
+	v1.Get("/users/:id", h.Middleware.AuthRequired(), h.User.GetUser)
+	v1.Put("/users/:id", h.Middleware.AuthRequired(), h.User.Update)
+	v1.Delete("/users/:id", h.Middleware.AuthRequired(), h.User.Delete)
 
 	return app
 }

db57df0fa653a96fdf1f25c0e1dfb52445cf5a13
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-03 17:35:06 +0700

    rename property fullname to full_name

--- app/dto/auth.go
+++ app/dto/auth.go
@@ -1,7 +1,7 @@
 package dto
 
 type RegisterInput struct {
-	Fullname string `json:"fullname" validate:"required"`
+	Fullname string `json:"full_name" validate:"required"`
 	Username string `json:"username" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=6"`

202372e01e4e61cc2b3f3d02415a73607f064616
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-04 19:34:43 +0700

    refactor naming method and add new method for getArtistByIds

--- app/contracts/artist.go
+++ app/contracts/artist.go
@@ -7,7 +7,8 @@ import (
 )
 
 type ArtistRepository interface {
-	FetchAll(ctx context.Context, pageSize, offset int) (artists []models.Artist, err error)
+	FindAll(ctx context.Context, pageSize, offset int) (artists []models.Artist, err error)
+	FindByArtistIds(ctx context.Context, inClause string, artistIds []any) (artists []models.Artist, err error)
 	Count(ctx context.Context) (total int, err error)
 	Store(ctx context.Context, name, slug string, image []byte) (err error)
 	FindDuplicateArtistBySlug(ctx context.Context, slug string) (exists bool, err error)
@@ -18,6 +19,7 @@ type ArtistRepository interface {
 
 type ArtistService interface {
 	GetAll(ctx context.Context, pageSize, offset int) (artists []models.Artist, err error)
+	GetArtistByIds(ctx context.Context, inClause string, artistIds []any) (artists []models.Artist, err error)
 	GetCount(ctx context.Context) (total int, err error)
 	Create(ctx context.Context, name, slug string, image []byte) (err error)
 	CheckDuplicateArtistBySlug(ctx context.Context, slug string) (exists bool, err error)

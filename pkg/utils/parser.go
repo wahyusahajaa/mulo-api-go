989f95db47108efe350e83472cd505da4e2867c2
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-05 22:06:57 +0700

    add utils for parse body request to check json format

--- pkg/utils/parser.go
+++ pkg/utils/parser.go
@@ -3,6 +3,9 @@ package utils
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/wahyusahajaa/mulo-api-go/app/dto"
@@ -38,6 +41,52 @@ func ParseImageToByte(image *dto.Image) (imgByte []byte, err error) {
 	if image != nil {
 		imgByte, err = json.Marshal(image)
 		return
+	}
+
+	return nil, nil
+}
+
+// Parse request body for check json input
+func ParseBody(body []byte, out interface{}) (map[string]string, error) {
+	var raw map[string]json.RawMessage
+
+	if err := json.Unmarshal(body, &raw); err != nil {
+		return nil, fmt.Errorf("invalid JSON format: %w", err)
+	}
+
+	t := reflect.TypeOf(out).Elem()
+	v := reflect.ValueOf(out).Elem()
+
+	errorMap := make(map[string]string)
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldVal := v.Field(i)
+
+		jsonTag := field.Tag.Get("json")
+		if jsonTag == "" || jsonTag == "-" {
+			continue
+		}
+
+		jsonKey := strings.Split(jsonTag, ",")[0]
+		rawValue, ok := raw[jsonKey]
+		if !ok {
+			continue
+		}
+
+		// Try to unmarshal into the field
+		if err := json.Unmarshal(rawValue, fieldVal.Addr().Interface()); err != nil {
+			errorMap[jsonKey] = fmt.Sprintf("Expected type %s", field.Type.Name())
+		}
+	}
+
+	if len(errorMap) > 0 {
+		return errorMap, nil
+	}
+
+	// Full decode if all types are OK
+	if err := json.Unmarshal(body, out); err != nil {
+		return nil, fmt.Errorf("invalid request body: %w", err)
 	}
 
 	return nil, nil

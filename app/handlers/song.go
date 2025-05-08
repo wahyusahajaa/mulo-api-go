a230cbd5ca664bd80c9e716237e1c819474a34e3
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-09 06:17:05 +0700

    use HandleHTTPError for handle song error

--- app/handlers/song.go
+++ app/handlers/song.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"errors"
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
@@ -28,20 +27,12 @@ func (h *SongHandler) GetSongs(c *fiber.Ctx) error {
 
 	songs, err := h.svc.GetAll(c.Context(), pageSize, offset)
 	if err != nil {
-		utils.LogError(h.log, c.Context(), "song_handler", "GetSongs", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "GetSongs", err)
 	}
 
 	total, err := h.svc.GetCount(c.Context())
 	if err != nil {
-		utils.LogError(h.log, c.Context(), "song_handler", "GetSongs", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "GetSongs", err)
 	}
 
 	return c.JSON(fiber.Map{
@@ -59,19 +50,7 @@ func (h *SongHandler) GetSong(c *fiber.Ctx) error {
 
 	song, err := h.svc.GetSongById(c.Context(), id)
 	if err != nil {
-		var notFoundErr utils.NotFoundError
-		if errors.As(err, &notFoundErr) {
-			utils.LogWarn(h.log, c.Context(), "song_handler", "GetSong", notFoundErr)
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": notFoundErr.Error(),
-			})
-		}
-
-		utils.LogError(h.log, c.Context(), "song_handler", "GetSong", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "GetSong", err)
 	}
 
 	return c.JSON(fiber.Map{
@@ -82,49 +61,14 @@ func (h *SongHandler) GetSong(c *fiber.Ctx) error {
 func (h *SongHandler) CreateSong(c *fiber.Ctx) error {
 	var req dto.CreateSongRequest
 
-	errorsMap, err := utils.ParseBody(c.Body(), &req)
-	if err != nil {
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": err.Error(),
-		})
-	}
-
-	if errorsMap != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid data type",
-			"errors":  errorsMap,
+			"message": "Invalid body request.",
 		})
 	}
 
 	if err := h.svc.CreateSong(c.Context(), req); err != nil {
-		valErr := utils.BadReqError{}
-		if errors.As(err, &valErr) {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "Validation failed",
-				"errors":  valErr.Errors,
-			})
-		}
-
-		notFoundErr := utils.NotFoundError{}
-		if errors.As(err, &notFoundErr) {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": notFoundErr.Error(),
-			})
-		}
-
-		conflictErr := utils.ConflictError{}
-		if errors.As(err, &conflictErr) {
-			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-				"message": conflictErr.Error(),
-			})
-		}
-
-		utils.LogError(h.log, c.Context(), "song_handler", "CreateSong", err)
-
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "CreateSong", err)
 	}
 
 	return c.JSON(fiber.Map{
@@ -136,43 +80,14 @@ func (h *SongHandler) UpdateSong(c *fiber.Ctx) error {
 	var req dto.CreateSongRequest
 	id, _ := strconv.Atoi(c.Params("id"))
 
-	errorsMap, err := utils.ParseBody(c.Body(), &req)
-	if err != nil {
+	if err := c.BodyParser(&req); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": err.Error(),
-		})
-	}
-
-	if errorsMap != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid data type",
-			"errors":  errorsMap,
+			"message": "Invalid body request.",
 		})
 	}
 
 	if err := h.svc.UpdateSong(c.Context(), req, id); err != nil {
-
-		valErr := utils.BadReqError{}
-		if errors.As(err, &valErr) {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": "Validation failed",
-				"errors":  valErr.Errors,
-			})
-		}
-
-		notFoundErr := utils.NotFoundError{}
-		if errors.As(err, &notFoundErr) {
-			utils.LogWarn(h.log, c.Context(), "song_handler", "UpdateSong", notFoundErr)
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"message": notFoundErr.Error(),
-			})
-		}
-
-		utils.LogError(h.log, c.Context(), "song_handler", "UpdateSong", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "UpdateSong", err)
 	}
 
 	return c.JSON(fiber.Map{
@@ -184,20 +99,7 @@ func (h *SongHandler) DeleteSong(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 
 	if err := h.svc.DeleteSong(c.Context(), id); err != nil {
-
-		notFoundErr := utils.NotFoundError{}
-		if errors.As(err, &notFoundErr) {
-			utils.LogWarn(h.log, c.Context(), "song_handler", "DeleteSong", notFoundErr)
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-				"message": notFoundErr.Error(),
-			})
-		}
-
-		utils.LogError(h.log, c.Context(), "song_handler", "DeleteSong", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message":   fiber.ErrInternalServerError.Message,
-			"requestId": utils.GetRequestId(c.Context()),
-		})
+		return utils.HandleHTTPError(c, h.log, "song_handler", "DeleteSong", err)
 	}
 
 	return c.JSON(fiber.Map{

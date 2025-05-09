d1f1c6edfda692978734fa70d97ad507dbe27a0f
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-09 09:04:44 +0700

    adjust response albums dto, and add new contract for get albums by artist id

--- app/contracts/album.go
+++ app/contracts/album.go
@@ -16,13 +16,15 @@ type AlbumRepository interface {
 	Store(ctx context.Context, input models.CreateAlbumInput) (err error)
 	Update(ctx context.Context, input models.CreateAlbumInput, id int) (err error)
 	Delete(ctx context.Context, id int) (err error)
+	FindAlbumsByArtistId(ctx context.Context, artistId int) (albums []models.Album, err error)
 }
 
 type AlbumService interface {
-	GetAll(ctx context.Context, pageSize, offset int) (albums []dto.Album, err error)
-	GetAlbumById(ctx context.Context, id int) (album dto.Album, err error)
+	GetAll(ctx context.Context, pageSize, offset int) (albums []dto.AlbumWithArtist, err error)
+	GetAlbumById(ctx context.Context, id int) (album dto.AlbumWithArtist, err error)
 	GetCount(ctx context.Context) (total int, err error)
 	CreateAlbum(ctx context.Context, req dto.CreateAlbumRequest) (err error)
 	UpdateAlbum(ctx context.Context, req dto.CreateAlbumRequest, id int) (err error)
 	DeleteAlbum(ctx context.Context, id int) (err error)
+	GetAlbumsByArtistId(ctx context.Context, artistId int) (albums []dto.Album, err error)
 }

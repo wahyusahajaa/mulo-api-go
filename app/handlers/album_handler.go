3b428d173a08f98645110462507b46096aa8591e
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-15 18:00:10 +0700

    add swagger docs on albums feature

--- app/handlers/album_handler.go
+++ app/handlers/album_handler.go
@@ -23,22 +23,27 @@ func NewAlbumHandler(svc contracts.AlbumService, log *logrus.Logger) *AlbumHandl
 	}
 }
 
+// @Summary      	List Albums
+// @Description  	Get paginated list of albums
+// @Tags         	albums
+// @Security     	BearerAuth
+// @Produce      	json
+// @Param        	page     	query    	int  false  "Page number" default(1)
+// @Param        	pageSize 	query    	int  false  "Page size" default(10)
+// @Success 		200 		{object}	dto.ResponseWithPagination[[]dto.Album, dto.Pagination]
+// @Failure 		500			{object}	dto.InternalErrorResponse "Internal server error"
+// @Router      	/albums [get]
 func (h *AlbumHandler) GetAlbums(c *fiber.Ctx) error {
 	page, pageSize, offset := utils.GetPaginationParam(c)
 
-	albums, err := h.svc.GetAll(c.Context(), pageSize, offset)
+	albums, total, err := h.svc.GetAll(c.Context(), pageSize, offset)
 	if err != nil {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "GetAlbums", err)
 	}
 
-	total, err := h.svc.GetCount(c.Context())
-	if err != nil {
-		return errs.HandleHTTPError(c, h.log, "album_handler", "GetAlbums", err)
-	}
-
-	return c.JSON(fiber.Map{
-		"data": albums,
-		"pagination": dto.Pagination{
+	return c.JSON(dto.ResponseWithPagination[[]dto.AlbumWithArtist, dto.Pagination]{
+		Data: albums,
+		Pagination: dto.Pagination{
 			Total:    total,
 			Page:     page,
 			PageSize: pageSize,
@@ -46,6 +51,16 @@ func (h *AlbumHandler) GetAlbums(c *fiber.Ctx) error {
 	})
 }
 
+// @Summary      	Get Album by ID
+// @Description  	Get a Album by their ID
+// @Tags        	albums
+// @Security     	BearerAuth
+// @Produce      	json
+// @Param        	id		path     	int	true  "Album ID"
+// @Success 		200		{object} 	dto.ResponseWithData[dto.Album]
+// @Failure 		404 	{object} 	dto.ErrorResponse "Album not found"
+// @Failure 		500 	{object} 	dto.InternalErrorResponse "Internal server error"
+// @Router       	/albums/{id} [get]
 func (h *AlbumHandler) GetAlbum(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 
@@ -54,17 +69,30 @@ func (h *AlbumHandler) GetAlbum(c *fiber.Ctx) error {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "GetAlbum", err)
 	}
 
-	return c.JSON(fiber.Map{
-		"data": album,
+	return c.JSON(dto.ResponseWithData[dto.AlbumWithArtist]{
+		Data: album,
 	})
 }
 
+// @Summary 		Create album
+// @Description 	Create a new album.
+// @Tags        	albums
+// @Security     	BearerAuth
+// @Accept 			json
+// @Produce 		json
+// @Param 			album	 body		dto.CreateAlbumRequest true "Album object that needs to be created"
+// @Success 		201 	{object} 	dto.ResponseMessage
+// @Failure 		400		{object} 	dto.ValidationErrorResponse "Invalid request"
+// @Failure 		404		{object} 	dto.ValidationErrorResponse "Not Found"
+// @Failure 		409		{object} 	dto.ValidationErrorResponse "Conflict album name"
+// @Failure 		500 	{object} 	dto.InternalErrorResponse "Internal server error"
+// @Router 			/albums [post]
 func (h *AlbumHandler) CreateAlbum(c *fiber.Ctx) error {
 	var req dto.CreateAlbumRequest
 
 	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid body request.",
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ResponseError{
+			Message: "Invalid body request.",
 		})
 	}
 
@@ -72,18 +100,32 @@ func (h *AlbumHandler) CreateAlbum(c *fiber.Ctx) error {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "CreateAlbum", err)
 	}
 
-	return c.JSON(fiber.Map{
-		"message": "Suceessfully created album",
+	return c.JSON(dto.ResponseMessage{
+		Message: "Suceessfully created album.",
 	})
 }
 
+// @Summary 		Update album
+// @Description 	Update the album with the specified ID
+// @Tags        	albums
+// @Security     	BearerAuth
+// @Accept 			json
+// @Produce 		json
+// @Param 			id 		path int true "Album ID"
+// @Param 			album	body		dto.CreateAlbumRequest true "album object that needs to be updated"
+// @Success 		200 	{object} 	dto.ResponseMessage
+// @Failure 		400		{object} 	dto.ValidationErrorResponse "Invalid request"
+// @Failure 		404 	{object} 	dto.ErrorResponse "Not Found: album or artist not found"
+// @Failure 		409		{object} 	dto.ValidationErrorResponse "Conflict: album name"
+// @Failure 		500 	{object} 	dto.InternalErrorResponse "Internal server error"
+// @Router 			/albums/{id} [put]
 func (h *AlbumHandler) UpdateAlbum(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 	var req dto.CreateAlbumRequest
 
 	if err := c.BodyParser(&req); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "Invalid body request.",
+		return c.Status(fiber.StatusBadRequest).JSON(dto.ResponseError{
+			Message: "Invalid body request.",
 		})
 	}
 
@@ -91,11 +133,22 @@ func (h *AlbumHandler) UpdateAlbum(c *fiber.Ctx) error {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "UpdateAlbum", err)
 	}
 
-	return c.JSON(fiber.Map{
-		"message": "Successfully updated album",
+	return c.JSON(dto.ResponseMessage{
+		Message: "Successfully updated album.",
 	})
 }
 
+// @Summary 		Delete album
+// @Description 	Delete the album with the specified ID
+// @Tags        	albums
+// @Security     	BearerAuth
+// @Accept 			json
+// @Produce 		json
+// @Param 			id path int true "album ID"
+// @Success 		200		{object} 	dto.ResponseMessage
+// @Failure 		404 	{object} 	dto.ErrorResponse "Not Found: Album not found"
+// @Failure 		500 	{object} 	dto.InternalErrorResponse "Internal server error"
+// @Router 			/albums/{id} [delete]
 func (h *AlbumHandler) DeleteAlbum(c *fiber.Ctx) error {
 	id, _ := strconv.Atoi(c.Params("id"))
 
@@ -103,20 +156,20 @@ func (h *AlbumHandler) DeleteAlbum(c *fiber.Ctx) error {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "DeleteAlbum", err)
 	}
 
-	return c.JSON(fiber.Map{
-		"message": "Successfully deleted album",
+	return c.JSON(dto.ResponseMessage{
+		Message: "Successfully deleted album.",
 	})
 }
 
 func (h *AlbumHandler) GetAlbumsByArtistId(c *fiber.Ctx) error {
 	artistId, _ := strconv.Atoi(c.Params("id"))
 
-	artists, err := h.svc.GetAlbumsByArtistId(c.Context(), artistId)
+	albums, err := h.svc.GetAlbumsByArtistId(c.Context(), artistId)
 	if err != nil {
 		return errs.HandleHTTPError(c, h.log, "album_handler", "GetAlbumsByArtistId", err)
 	}
 
-	return c.JSON(fiber.Map{
-		"data": artists,
+	return c.JSON(dto.ResponseWithData[[]dto.Album]{
+		Data: albums,
 	})
 }

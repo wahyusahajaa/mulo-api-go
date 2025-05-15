c77d88bdc4a2eca7279fc41aca02e34b1ec9337d
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-15 13:02:54 +0700

    add description on song service methods

--- app/contracts/song_contract.go
+++ app/contracts/song_contract.go
@@ -18,10 +18,38 @@ type SongRepository interface {
 }
 
 type SongService interface {
-	GetAll(ctx context.Context, pageSize, offset int) (songs []dto.Song, err error)
-	GetCount(ctx context.Context) (total int, err error)
+	// GetAll Return list of songs and total.
+	//  Returns:
+	//   200 OK: Success with lists and total.
+	//   500 Internal Server Error: On Failure.
+	GetAll(ctx context.Context, pageSize, offset int) (songs []dto.Song, total int, err error)
+
+	// GetSongById Retrieve a song by Id.
+	//  Returns:
+	//   200 OK: on success with a song.
+	//   400 Not Found if song does not exist.
+	//   500 Internal Server Error: on failure.
 	GetSongById(ctx context.Context, id int) (song dto.Song, err error)
+
+	// CreateSong insert a new song.
+	//  Returns:
+	//   201 Created: on success.
+	//   400 Bad Request: on validation failure.
+	//   500 Internal Server Error: on failure.
 	CreateSong(ctx context.Context, req dto.CreateSongRequest) (err error)
+
+	// UpdateSong update an existing song by ID.
+	//  Returns:
+	//   200 OK: on success.
+	//   400 Bad Request: on validation failure.
+	//   404 Not Found: if song is missing.
+	//   500 Internal Server Error: on failure.
 	UpdateSong(ctx context.Context, req dto.CreateSongRequest, id int) (err error)
+
+	// DeleteSong remove a song by ID.
+	//  Returns:
+	//   200 OK: on success.
+	//   404 Not Found: if song is missing.
+	//   500 Internal Server Error: on failure.
 	DeleteSong(ctx context.Context, id int) (err error)
 }

bef7564c5f0f95e347e1ef668671a438283429c3
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-15 17:56:09 +0700

    add comment for AlbumService methods

--- app/contracts/album_contract.go
+++ app/contracts/album_contract.go
@@ -20,11 +20,47 @@ type AlbumRepository interface {
 }
 
 type AlbumService interface {
-	GetAll(ctx context.Context, pageSize, offset int) (albums []dto.AlbumWithArtist, err error)
+	// GetAll Return list of albums and total.
+	//  Returns:
+	//   200 OK: Success with lists and total.
+	//   500 Internal Server Error: On Failure.
+	GetAll(ctx context.Context, pageSize, offset int) (albums []dto.AlbumWithArtist, total int, err error)
+
+	// GetAlbumById Retrieve a album by Id.
+	//  Returns:
+	//   200 OK: on success with a album.
+	//   400 Not Found: album does not exists.
+	//   500 Internal Server Error: on failure.
 	GetAlbumById(ctx context.Context, id int) (album dto.AlbumWithArtist, err error)
-	GetCount(ctx context.Context) (total int, err error)
+
+	// CreateAlbum insert a new album.
+	//  Returns:
+	//   201 Created: on success.
+	//   400 Bad Request: on validation failure.
+	//   404 Not Found: artist does not exists.
+	//   409 Conflict: album name or slug already exists.
+	//   500 Internal Server Error: on failure.
 	CreateAlbum(ctx context.Context, req dto.CreateAlbumRequest) (err error)
+
+	// UpdateAlbum update an existing album by ID.
+	//  Returns:
+	//   200 OK: on success.
+	//   400 Bad Request: on validation failure.
+	//   404 Not Found: album or artist does not exists.
+	//   409 Conflict: album name or slug already exists.
+	//   500 Internal Server Error: on failure.
 	UpdateAlbum(ctx context.Context, req dto.CreateAlbumRequest, id int) (err error)
+
+	// DeleteAlbum remove a album by ID.
+	//  Returns:
+	//   200 OK: on success.
+	//   404 Not Found: album does not exists.
+	//   500 Internal Server Error: on failure.
 	DeleteAlbum(ctx context.Context, id int) (err error)
+
+	// GetAll Return list of albums by artist id.
+	//  Returns:
+	//   200 OK: Success with lists.
+	//   500 Internal Server Error: On Failure.
 	GetAlbumsByArtistId(ctx context.Context, artistId int) (albums []dto.Album, err error)
 }

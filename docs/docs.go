9b2e42af836d8520cb56ccde3f33d4eec9800549
wahyusahajaa <wahyusahaja.official@gmail.com>
2025-05-28 09:43:02 +0700

    update swagger docs

--- docs/docs.go
+++ docs/docs.go
@@ -3427,7 +3427,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "api.mulo.site",
 	BasePath:         "/v1",
 	Schemes:          []string{"http", "https"},
 	Title:            "Mulo Music Streaming API",
